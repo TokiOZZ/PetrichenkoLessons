2d117933deb9ecaaf9117c1bc3866dc072a9820f
TokiOZZ <trotsenkop@gmail.com>
2021-12-01 10:13:19 +0200

    Fixed some problems with repo

--- script.js
+++ script.js
@@ -4,42 +4,58 @@
 'Сколько фильмов вы уже посмотрели?'
 
 2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
+	 - count - сюда передается ответ на первый вопрос
+	 - movies - в это свойство поместить пустой объект
+	 - actors - тоже поместить пустой объект
+	 - genres - сюда поместить пустой массив
+	 - privat - в это свойство поместить boolean(логическое) значение false
 
 3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
+	 - 'Один из последних просмотренных фильмов?'
+	 - 'На сколько оцените его?'
 Ответы стоит поместить в отдельные переменные
 Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
+	 movies: {
+		  'logan': '8.1'
+	 }
 
 Проверить, чтобы все работало без ошибок в консоли */
 
 'use strict';
 // 1 task
-const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
 // 2 task
 const personalMovieDB = {
-    'count' : numberOfFilms,
-    'movies' : {},
-    'actors' : {},
-    'genres' : [],
-    'privat' : false,
+	'count': numberOfFilms,
+	'movies': {},
+	'actors': {},
+	'genres': [],
+	'privat': false,
 };
 // 3 task
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = +prompt('На сколько оцените его?', '');
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
 
-console.log(personalMovieDB);
+	const a = prompt('Один из последних просмотренных фильмов?', '');
+	const b = prompt('На сколько оцените его?', '');
+
+	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+		console.log(a, b);
+		personalMovieDB.movies[a] = b;
+	} else {
+		console.log('It does`t work..');
+		i--;
+	}
+}
+
+if (personalMovieDB.count < 10) {
+	console.log('Просмотрено довольно мало фильмов')
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log('Вы классический зритель')
+} else if (personalMovieDB.count >= 30) {
+	console.log('Вы киноман')
+} else console.log('Произошла ошибка')
+
+
 
